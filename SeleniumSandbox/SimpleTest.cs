8049b1abdaccfc6fedf021de746b6c0842d809a1
jeff-crowley <jeffrey.crowley@harlandclarke.com>
2018-04-28 08:59:08 -0500

    Added some tests

---
+++ SeleniumSandbox/SimpleTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace SeleniumSandbox
+{
+    [TestFixture]
+    class SimpleTest : Base
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Fail("This is a failure");
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.True(2 == 2);
+        }
+        [Test]
+        public void Test3()
+        {
+            Assert.True(2 == 2);
+        }
+        [Test]
+        public void Test4()
+        {
+            Assert.True(2 == 2);
+        }
+    }
+}
--- SeleniumSandbox/UnitTest1.cs
+++ SeleniumSandbox/UnitTest1.cs
@@ -1,14 +1,52 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace SeleniumSandbox
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
+        [Test]
         public void TestMethod1()
         {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+            driver.Url = "http://www.google.com";
+
+        }
+        [Test]
+        public void ExtentTest()
+        {
+            var dir = TestContext.CurrentContext.TestDirectory + "\\";
+
+            // start reporters
+            var htmlReporter = new ExtentHtmlReporter(dir + "extent.html");
+
+            // create ExtentReports and attach reporter(s)
+            var extent = new ExtentReports();
+            extent.AttachReporter(htmlReporter);
+
+            // creates a toggle for the given test, adds all log events under it    
+            var test = extent.CreateTest("MyFirstTest", "Sample description");
+
+            // log(Status, details)
+            test.Log(Status.Info, "This step shows usage of log(status, details)");
+
+            // info(details)
+            test.Info("This step shows usage of info(details)");
+
+            // log with snapshot
+            test.Fail("details", MediaEntityBuilder.CreateScreenCaptureFromPath("screenshot.png").Build());
+
+            // test with snapshot
+            test.AddScreenCaptureFromPath("screenshot.png");
+
+            // calling flush writes everything to the log file
+            extent.Flush();
         }
     }
 }
