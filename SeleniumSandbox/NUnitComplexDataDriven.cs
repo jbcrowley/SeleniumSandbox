6083bd4925c9707dfa19fcbf9e396e999ba2a3ef
jeff-crowley <jeffrey.crowley@harlandclarke.com>
2018-06-17 13:29:05 -0500

    Added some data driven tests 1. Added a simple data driven test 2. Added a more complex data source for a data driven test

---
+++ SeleniumSandbox/NUnitComplexDataDriven.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Reflection;
+using SC = System.ComponentModel;
+
+namespace SeleniumSandbox
+{
+    [TestFixture]
+    public class NUnitComplexDataDriven
+    {
+        [Test, TestCaseSource(typeof(TestData), "OtherVehicleLoanCases")]
+        public void NUnitDataDriven(User user, OtherVehicleLoanData otherAutoLoanData)
+        {
+            System.Console.WriteLine($"{user.username}, {user.password}, {otherAutoLoanData.type.GetDescription()}, {otherAutoLoanData.rate}");
+        }
+    }
+
+    public class TestData
+    {
+        public static IEnumerable OtherVehicleLoanCases
+        {
+            get
+            {
+                yield return new TestCaseData(new User(username: "121021", password: "Passw0rd"), new OtherVehicleLoanData(type: OtherVehicleLoanType.Boat, rate: "1.5"));
+                yield return new TestCaseData(new User(username: "123456", password: "Passw0rd"), new OtherVehicleLoanData(type: OtherVehicleLoanType.Motorcycle, rate: "9.5"));
+                yield return new TestCaseData(new User(username: "234567", password: "Passw0rd"), new OtherVehicleLoanData(type: OtherVehicleLoanType.RV, rate: "11.5"));
+            }
+        }
+    }
+
+    public class User
+    {
+        public string username;
+        public string password;
+
+        public User(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+    }
+
+    public class OtherVehicleLoanData
+    {
+        public OtherVehicleLoanType type;
+        public string rate;
+
+        public OtherVehicleLoanData(OtherVehicleLoanType type, string rate)
+        {
+            this.type = type;
+            this.rate = rate;
+        }
+    }
+
+    public enum OtherVehicleLoanType
+    {
+        Boat,
+        [SC.Description("Recreational Vehicle")]
+        RV,
+        Motorcycle
+    }
+
+    public static class Extensions
+    {
+        public static string GetDescription(this Enum value)
+        {
+            FieldInfo fieldInfo = value.GetType().GetField(value.ToString());
+            var attribute = (SC.DescriptionAttribute)fieldInfo.GetCustomAttribute(typeof(SC.DescriptionAttribute));
+
+            return (attribute != null) ? attribute.Description : value.ToString();
+        }
+    }
+}
---
+++ SeleniumSandbox/NUnitSimpleDataDriven.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System.Collections;
+
+namespace SeleniumSandbox
+{
+    [TestFixture]
+    public class NUnitSimpleDataDriven
+    {
+        [Test, TestCaseSource(typeof(MySimpleDataClass), "TestCases")]
+        public void NUnitDataDriven(string username, string password)
+        {
+            System.Console.WriteLine($"{username}, {password}");
+        }
+    }
+
+    public class MySimpleDataClass
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData("121021", "Passw0rd");
+                yield return new TestCaseData("123456", "Passw0rd");
+                yield return new TestCaseData("234567", "Passw0rd");
+                yield return new TestCaseData("345678", "Passw0rd");
+            }
+        }
+    }
+}
