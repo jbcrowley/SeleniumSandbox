284b80cbc2baecae758f8216632976c6cd537d9a
jeff-crowley <jeffrey.crowley@harlandclarke.com>
2018-09-28 14:39:37 -0500

    Quick update

---
+++ SeleniumSandbox/Tests/Sandbox.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SeleniumSandbox.Tests
+{
+    [TestFixture]
+    public class Sandbox
+    {
+        IWebDriver Driver;
+
+        [Test]
+        public void SampleTest()
+        {
+            Driver = new ChromeDriver();
+            Driver.Manage().Window.Maximize();
+            string url = "https://www.humanbenchmark.com/tests/reactiontime/";
+            Driver.Url = url;
+        }
+    }
+}
